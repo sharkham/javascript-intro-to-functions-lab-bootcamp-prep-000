5111541517b7b431399c3b74c56e63cf44504d4e
Sam Markham <sam.e.markham@gmail.com>
2019-04-22 23:22:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,14 +11,14 @@ function logShout(string) {
   console.log(string.toUpperCase)
 };
 
-describe('logShout(string)', function() {
-  it('calls console.log() its one argument in all caps', function() {
-    const spy = expect.spyOn(console, 'log').andCallThrough()
+//describe('logShout(string)', function() {
+ // it('calls console.log() its one argument in all caps', function() {
+   // const spy = expect.spyOn(console, 'log').andCallThrough()
 
-    logShout('hello')
+   // logShout('hello')
 
-    expect(spy).toHaveBeenCalledWith('HELLO')
+   // expect(spy).toHaveBeenCalledWith('HELLO')
 
-    console.log.restore()
-  })
-})
+  //  console.log.restore()
+  //})
+//})
