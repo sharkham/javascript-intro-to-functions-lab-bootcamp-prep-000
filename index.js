3ad7db580ad4ebfa53f3e2a714efe522d753271a
Sam Markham <sam.e.markham@gmail.com>
2019-04-22 23:48:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,14 +11,7 @@ function logShout(string) {
 };
 logShout('hello');
 
-//describe('logShout(string)', function() {
- // it('calls console.log() its one argument in all caps', function() {
-   // const spy = expect.spyOn(console, 'log').andCallThrough()
-
-   // logShout('hello')
-
-   // expect(spy).toHaveBeenCalledWith('HELLO')
-
-  //  console.log.restore()
-  //})
-//})
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+};
+logWhisper('HELLO');
